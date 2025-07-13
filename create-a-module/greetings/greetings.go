8333b59e7db7bd82e5beaf264deb705fc5cb30c6
28604 <irvingchen930701@gmail.com>
2025-07-14 00:00:13 +0800

    Add Randomness

--- create-a-module/greetings/greetings.go
+++ create-a-module/greetings/greetings.go
@@ -3,6 +3,7 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 )
 
 // A function name starting with a capital letter can be called by a function not in the same package.
@@ -17,6 +18,21 @@ func Hello(name string) (string, error) {
 	}
 	// If a name was given, return a greeting that embeds the name in a message.
 	// Sprintf returns a formatted string.
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+// randomFormat, an unexported function, randomly returns one of the greeting messages.
+func randomFormat() string {
+	// A slice of message formats.
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+	// Return a randomly selected message format by specifying
+	// a random index for the slice of formats.
+
+	// Intn returns a non-negative pseudo-random number in [0,n).
+	return formats[rand.Intn(len(formats))]
 }
