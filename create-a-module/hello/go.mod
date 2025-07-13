e91844f971cc9bd871f22d2549bd6504957ff9c5
28604 <irvingchen930701@gmail.com>
2025-07-13 23:24:27 +0800

    Call from Another Module

--- create-a-module/hello/go.mod
+++ create-a-module/hello/go.mod
@@ -1,3 +1,7 @@
 module github.com/28604/go-doc-tutorial/create-a-module/hello
 
 go 1.24.4
+
+replace github.com/28604/go-doc-tutorial/create-a-module/greetings => ../greetings
+
+require github.com/28604/go-doc-tutorial/create-a-module/greetings v0.0.0-00010101000000-000000000000
--- create-a-module/hello/hello.go
+++ create-a-module/hello/hello.go
@@ -2,9 +2,15 @@ package main
 
 import (
 	"fmt"
+	// The way to import greetings if we actually want to download from GitHub.
+	"github.com/28604/go-doc-tutorial/create-a-module/greetings"
 )
 
 func main() {
 	message := greetings.Hello("Chen")
 	fmt.Println(message)
 }
+
+// However, if we want to find the greetings package locally, we run
+// $ go mod edit -replace github.com/28604/go-doc-tutorial/create-a-module/greetings=../greetings
+// $ go mod tidy
